ddb2258c20e34875487804556595cf05b3f8df6d
Noah Kim <nk8na@virginia.edu>
2017-01-31 14:10:01 -0500

    saved notes

---
+++ app/src/main/java/com/noahdkim/castor/MainActivity.java
@@ -0,0 +1,128 @@
+package com.noahdkim.castor;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.os.Environment;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+
+
+import java.io.File;
+
+public class MainActivity extends AppCompatActivity {
+
+    static int numNote = 0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestPermissions();
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton addNote = (FloatingActionButton) findViewById(R.id.addNote);
+        final Context context = this;
+        final Intent intent = new Intent(MainActivity.this, Note.class);
+        addNote.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                startActivity(intent);
+
+
+            }
+        });
+    }
+    protected void onResume(){
+        super.onResume();
+        LinearLayout noteList = (LinearLayout) findViewById(R.id.Menu);
+        noteList.removeAllViews();
+        final String[] noteTitles = getNotes();
+        numNote = noteTitles.length;
+        final Context context = this;
+        final Intent intent = new Intent(MainActivity.this, Note.class);
+        for(int i = 0; i<numNote; i++){
+            Button toAdd = new Button(this);
+            Toolbar.LayoutParams lp = new Toolbar.LayoutParams(Toolbar.LayoutParams.MATCH_PARENT, Toolbar.LayoutParams.WRAP_CONTENT);
+            toAdd.setId(numNote);
+            toAdd.setText(noteTitles[i]);
+            System.out.println(noteTitles[i]);
+            final int noteIndex = i;
+            toAdd.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    intent.putExtra("title",noteTitles[noteIndex]);
+                    startActivity(intent);
+                }
+            });
+
+            noteList.addView(toAdd,lp); // noteList is the container of the buttons
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+    public void requestPermissions(){
+        ActivityCompat.requestPermissions(MainActivity.this,
+                new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                1);
+    }
+
+    public String[] getNotes(){
+        File sdCard = Environment.getExternalStorageDirectory();
+        File directory = new File (sdCard.getAbsolutePath() + "/MyFiles");
+        File[] listOfNotes =  directory.listFiles();
+        int noteCount = listOfNotes.length;
+        String[] returnList = new String[noteCount];
+        for(int i = 0; i<listOfNotes.length; i++){
+            if (listOfNotes[i].isFile()) {
+                String fname = listOfNotes[i].getName();
+                int pos = fname.lastIndexOf(".");
+                if (pos > 0) {
+                    fname = fname.substring(0, pos);
+                }
+                returnList[i] = fname;
+                System.out.println("File " + listOfNotes[i].getName());
+            } else if (listOfNotes[i].isDirectory()) {
+                returnList[i] = listOfNotes[i].getName();
+            }
+        }
+        return returnList;
+    }
+
+
+
+}
--- app/src/main/java/com/noahdkim/castor/Note.java
+++ app/src/main/java/com/noahdkim/castor/Note.java
@@ -1,13 +1,147 @@
 package com.noahdkim.castor;
 
+import android.content.Context;
+import android.content.Intent;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.EditText;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+
 
 public class Note extends AppCompatActivity {
 
+    private EditText editBody;
+    private EditText editTitle;
+    public static String initialTitle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_note);
+        Intent intent = getIntent();
+        initialTitle = intent.getStringExtra("title");
+        String title = initialTitle;
+        editTitle = (EditText) findViewById(R.id.title);
+        editTitle.setText(title);
+        if(isExternalStorageReadable()){
+            String noteBody = readFromSDFile(title);
+            System.out.println("note body:" + noteBody);
+            editBody = (EditText) findViewById(R.id.body);
+            editBody.setText(noteBody);
+        }
     }
+
+    protected void onStop() {
+        editBody = (EditText) findViewById(R.id.body);
+        editTitle = (EditText) findViewById(R.id.title);
+        String writeBody = editBody.getText().toString();
+        String finalTitle = editTitle.getText().toString();
+        Log.d("SAVE", writeBody);
+        if(isExternalStorageWritable()) {
+            Log.d("SAVE", "writable");
+            writeToSDFile(finalTitle, writeBody);
+        }
+        else{
+            Log.d("SAVE", "not writable");
+        }
+
+
+        super.onStop();
+    }
+
+
+
+    /* Checks if external storage is available for read and write */
+    public boolean isExternalStorageWritable() {
+        String state = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(state)) {
+            return true;
+        }
+        return false;
+    }
+
+    /* Checks if external storage is available to at least read */
+    public boolean isExternalStorageReadable() {
+        String state = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(state) ||
+                Environment.MEDIA_MOUNTED_READ_ONLY.equals(state)) {
+            return true;
+        }
+        return false;
+    }
+
+    private void writeToSDFile(String title, String toWrite){
+        try {
+            //This will get the SD Card directory and create a folder named MyFiles in it.
+            File sdCard = Environment.getExternalStorageDirectory();
+            File directory = new File (sdCard.getAbsolutePath() + "/MyFiles");
+
+            if(directory.exists()) {
+                Log.d("FILE", "found directory");
+            }
+            else{
+                directory.mkdirs();
+                Log.d("FILE", "directory not found");
+            }
+//Now create the file in the above directory and write the contents into it
+            File file = new File(directory, initialTitle + ".txt");
+            if(!title.equals(initialTitle)){
+                file.delete();
+                Log.d("FILE", "rename");
+                file = new File(directory, title + ".txt");
+
+            }
+
+            FileOutputStream fOut = new FileOutputStream(file);
+            OutputStreamWriter osw = new OutputStreamWriter(fOut);
+            osw.write(toWrite);
+            osw.flush();
+            osw.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            Log.d("FILE", "not found");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+    private String readFromSDFile(String title){
+
+        File sdCard = Environment.getExternalStorageDirectory();
+        File directory = new File (sdCard.getAbsolutePath() + "/MyFiles");
+        File file = new File(directory,title+".txt");
+        System.out.println(file.toString());
+        StringBuilder text = new StringBuilder();
+
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            String line;
+            while ((line = br.readLine()) != null) {
+                text.append(line);
+                text.append('\n');
+                System.out.println("text toString"+text.toString());
+            }
+            br.close();
+        }
+        catch (IOException e) {
+            //You'll need to add proper error handling here
+        }
+        return text.toString();
+    }
+
+
 }
+
+
+
